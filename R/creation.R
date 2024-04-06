6cb5a15659950406adcec295ac407f1983432552
stla <laurent_step@yahoo.fr>
2024-04-06 13:22:29 +0200

    rRatioOfQsprays

---
+++ R/creation.R
@@ -0,0 +1,13 @@
+#' @title Random 'ratioOfQsprays'
+#' @description Generates a random \code{ratioOfQsprays} object.
+#'
+#' @return A \code{ratioOfQsprays} object.
+#' @importFrom qspray rQspray
+rRatioOfQsprays <- function() {
+  numerator   <- rQspray()
+  denominator <- rQspray()
+  while(denominator == 0L) {
+    denominator <- rQspray()
+  }
+  numerator / denominator
+}
